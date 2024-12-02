8ecb299c43fd824231cf8c4e7fbd64e4d3ccdb08
shelton <sheltoncarvallo975@gmail.com>
2024-12-01 23:12:21 -0500

    Mostrar tablero inicial

---
+++ src/MainJuego.java
@@ -0,0 +1,8 @@
+import vista.TableroView;
+
+public class MainJuego {
+    public static void main(String[] args) {
+        TableroView view = new TableroView();
+        view.mostrarTablero();
+    }
+}
--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -1,5 +1,20 @@
 package modelo;
 
 public class Tablero {
+    private int dimensionesTablero;
+    private final char[][] tableroInicial = new char[10][10];
 
+
+    private void setTable() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                tableroInicial[i][j] = 'c';
+            }
+        }
+    }
+
+    public char[][] getTableroInicial() {
+        setTable();
+        return tableroInicial;
+    }
 }
